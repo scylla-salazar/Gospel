b1b1126f9c454f6dafd9f6e2423126f0542e6121
scylla-salazar <125595874+scylla-salazar@users.noreply.github.com>
2023-07-27 05:33:08 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -5,12 +5,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+#include <fcntl.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
 
 /* for read/write buffers */
 #define READ_BUF_SIZE 1024
@@ -38,8 +39,8 @@ extern char **environ;
 
 
 /**
- * struct liststr - singly linked list
- * @num: the number field
+ * struct liststr -singly_linked list
+ * @num: the num field
  *  @str: a str
  * @next: points to the next node
  */
@@ -51,8 +52,7 @@ typedef struct liststr
 } list_t;
 
 /**
- *struct passinfo - This contain pseudo-args to pass into a funct,
- *allowing uniform prototype for function ptr struct
+ *struct passinfo - has pseudo-args to pass into a funct, ptr struct
  *@arg: Refers a string generated from getline containing args
  *@argv: Refers to array of strs generated from arg
  *@path:  string path for the current cmd
@@ -63,8 +63,8 @@ typedef struct liststr
  *@err_num: the err code for exit()s
  *@linecount_flag: if on count this line of input.
  *@fname: the program filename;
- *@cmd_buf: add of pointer to cmd_buf, on if chaining
- *@cmd_buf_type: CMD_type ||, &&, ;
+ *@cmd_buf: add of ptr to cmd_buf, on if chaining
+ *@cmd_buf_type: CMD_type
  *@alias: refers the alias node;
  *@env_changed: on (if environ was changed)
  *@status: referrs to the return status of the last exec'd cmd
@@ -99,7 +99,6 @@ typedef struct passinfo
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
-/**
  *struct builtin - HAS a builtin str & funct
  *@type: the builtin cmd flag
  *@func: the function
@@ -115,115 +114,74 @@ int hsh(info_t *, char **);
 int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
-
-
-/* toem_parser.c */
 int is_cmd(info_t *, char *);
 char *dup_chars(char *, int, int);
 char *find_path(info_t *, char *, char *);
 
-/* loophsh.c ..*/
 int loophsh(char **);
-
-
-/* ;; toem_errors.c */
 void _eputs(char *);
 int _eputchar(char);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
-
-/* toem_string1.c */
 char *_strcpy(char *, char *);
 char *_strdup(const char *);
 void _puts(char *);
 int _putchar(char);
-
-/* toem_string.c */
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
 
-
-
-/*Loveline-Ezenwafor */
-
-
-/* toem_exits.c */
 char *_strncpy(char *, char *, int);
 char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
+char **str_tok(char *, char *);
+char **str_tok2(char *, char);
 
-/* toem_tokenizer.c */
-char **strtow(char *, char *);
-char **strtow2(char *, char);
-
-/*'' toem_realloc.c ..*/
 char *_memset(char *, char, unsigned int);
 void ffree(char **);
 void *_realloc(void *, unsigned int, unsigned int);
 
-
-/* toem_memory.c */
 int bfree(void **);
 
-
-/* ''toem_atoi.c */
 int interactive(info_t *);
 int is_delim(char, char *);
 int _isalpha(int);
 int _atoi(char *);
-
-
-/* toem_errors1.c */
 int _erratoi(char *);
 void print_error(info_t *, char *);
 int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 
-
-/* '' toem_builtin.c */
 int _myexit(info_t *);
 int _mycd(info_t *);
 int _myhelp(info_t *);
 
-
-/* ...toem_builtin1.c */
-int _myhistory(info_t *);
-int _myalias(info_t *);
-
-
-/*    toem_getline.c .. */
 ssize_t get_input(info_t *);
 int _getline(info_t *, char **, size_t *);
 void sigintHandler(int);
+int _myhistory(info_t *);
+int _myalias(info_t *);
 
-
-
-/* ..toem_getinfo.c   */
-void clear_info(info_t *);
-void set_info(info_t *, char **);
-void free_info(info_t *, int);
-
-
-/*  'toem_environ.c  */
 char *_getenv(info_t *, const char *);
 int _myenv(info_t *);
 int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
 
-/*scylla-salazar */
 
-/* .....toem_getenv.c  */
 char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
-
-
-
-/* '...toem_history.c */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
 char *get_history_file(info_t *info);
 int write_history(info_t *info);
 int read_history(info_t *info);
@@ -231,8 +189,6 @@ int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
 
-
-/*  'toem_lists.c  */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
@@ -240,15 +196,6 @@ int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
 
-/*' toem_lists1.c  */
-size_t list_len(const list_t *);
-char **list_to_strings(list_t *);
-size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
-
-
-/*  toem_vars.c'  */
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
